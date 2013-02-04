bd25f9e6bf04b900c32123220d0cb3095c15ea73
Adrien Herubel <herubel@gmail.com>
2013-02-04 23:04:23 +0100

    Initial commit

---
+++ GLCommon.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "glew/glew.h"
+#ifdef __APPLE__
+#include <OpenGL/gl3.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
---
+++ premake4.lua
@@ -0,0 +1,79 @@
+solution "imgui"
+   configurations { "Debug", "Release" }
+   platforms {"native", "x64", "x32"}
+
+  
+   -- imgui sample
+   project "sample"
+      kind "ConsoleApp"
+      language "C++"
+      files { "sample.cpp", "imgui.cpp",  "imguiRenderGL.cpp",  "imgui.h",  "imguiRenderGL.h",  "stb_truetype.h" }
+      includedirs { "lib/glfw/include", "src", "common", "lib/" }
+      links {"glfw", "glew"}
+      defines { "GLEW_STATIC" }
+     
+      configuration { "linux" }
+         links {"X11","Xrandr", "rt", "GL", "GLU", "pthread"}
+       
+      configuration { "windows" }
+         links {"glu32","opengl32", "gdi32", "winmm", "user32"}
+
+      configuration { "macosx" }
+         linkoptions { "-framework OpenGL", "-framework Cocoa", "-framework IOKit" }
+       
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols", "ExtraWarnings"}
+
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize", "ExtraWarnings"}    
+
+
+   -- GLFW Library
+   project "glfw"
+      kind "StaticLib"
+      language "C"
+      files { "lib/glfw/lib/*.h", "lib/glfw/lib/*.c", "lib/glfw/include/GL/glfw.h" }
+      includedirs { "lib/glfw/lib", "lib/glfw/include"}
+
+      configuration {"linux"}
+         files { "lib/glfw/lib/x11/*.c", "lib/glfw/x11/*.h" }
+         includedirs { "lib/glfw/lib/x11" }
+         defines { "_GLFW_USE_LINUX_JOYSTICKS", "_GLFW_HAS_XRANDR", "_GLFW_HAS_PTHREAD" ,"_GLFW_HAS_SCHED_YIELD", "_GLFW_HAS_GLXGETPROCADDRESS" }
+         buildoptions { "-pthread" }
+       
+      configuration {"windows"}
+         files { "lib/glfw/lib/win32/*.c", "lib/glfw/win32/*.h" }
+         includedirs { "lib/glfw/lib/win32" }
+         defines { "_GLFW_USE_LINUX_JOYSTICKS", "_GLFW_HAS_XRANDR", "_GLFW_HAS_PTHREAD" ,"_GLFW_HAS_SCHED_YIELD", "_GLFW_HAS_GLXGETPROCADDRESS" }
+       
+      configuration {"Macosx"}
+         files { "lib/glfw/lib/cocoa/*.c", "lib/glfw/lib/cocoa/*.h", "lib/glfw/lib/cocoa/*.m" }
+         includedirs { "lib/glfw/lib/cocoa" }
+         defines { }
+         buildoptions { " -fno-common" }
+         linkoptions { "-framework OpenGL", "-framework Cocoa", "-framework IOKit" }
+
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols", "ExtraWarnings" }
+
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize", "ExtraWarnings" }    
+
+   -- GLEW Library         
+   project "glew"
+      kind "StaticLib"
+      language "C"
+      files {"lib/glew/*.c", "lib/glew/*.h"}
+      defines { "GLEW_STATIC" }
+
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols", "ExtraWarnings" }
+
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize", "ExtraWarnings" }    
